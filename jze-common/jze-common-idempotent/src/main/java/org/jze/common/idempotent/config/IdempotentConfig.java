9d5de476ea8fa8a5d30149f2d2dbb6141a789cbe
Jay2Pink <20109240@qq.com>
2025-05-14 16:12:22 +0800

    init

---
+++ jze-common/jze-common-idempotent/src/main/java/org/jze/common/idempotent/config/IdempotentConfig.java
@@ -0,0 +1,21 @@
+package org.jze.common.idempotent.config;
+
+import org.jze.common.idempotent.aspectj.RepeatSubmitAspect;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConfiguration;
+
+/**
+ * 幂等功能配置
+ *
+ * @author jze
+ */
+@AutoConfiguration(after = RedisConfiguration.class)
+public class IdempotentConfig {
+
+    @Bean
+    public RepeatSubmitAspect repeatSubmitAspect() {
+        return new RepeatSubmitAspect();
+    }
+
+}
---
+++ jze-common/jze-common-log/src/main/java/org/jze/common/log/enums/BusinessStatus.java
@@ -0,0 +1,18 @@
+package org.jze.common.log.enums;
+
+/**
+ * 操作状态
+ *
+ * @author jze
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
